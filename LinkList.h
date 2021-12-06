a67911ea5a67ec7e8a340fa11ac2368f113d9dba
Cui-Yancy <813158377@qq.com>
2021-12-06 16:05:29 +0800

    define LinkList

---
+++ LinkList.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define FALSE		0
+#define TRUE		1
+#define OK		0
+#define ERROR		1
+
+typedef int ElemType;
+
+typedef struct ListNode
+{
+	ElemType data;
+	struct ListNode* next;
+}ListNode,*LinkList;
+
