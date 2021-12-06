e9fb3b1bc31c6a56e9cd1eb6b0f8931271f58321
Cui-Yancy <813158377@qq.com>
2021-12-06 17:06:44 +0800

    switch tab to space

--- LinkList.h
+++ LinkList.h
@@ -1,17 +1,17 @@
 #pragma once
 
-#define FALSE		0
-#define TRUE		1
-#define OK		0
-#define ERROR		1
+#define FALSE       0
+#define TRUE        1
+#define OK          0
+#define ERROR       1
 
 typedef int ElemType;
 typedef int status;
 
 typedef struct ListNode
 {
-	ElemType data;
-	struct ListNode* next;
+    ElemType data;
+    struct ListNode* next;
 }ListNode,*LinkList;
 
 status ListDelete(LinkList& L,int i);
