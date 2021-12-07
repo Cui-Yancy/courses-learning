a012a3140a6545145d330f3af34a836c2dc19c1d
Cui-Yancy <813158377@qq.com>
2021-12-07 16:31:16 +0800

    init my LinkList folder

---
+++ DataConstruct/LinkList/LinkList.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define FALSE       0
+#define TRUE        1
+#define OK          0
+#define ERROR       1
+
+typedef int ElemType;
+typedef int status;
+
+typedef struct ListNode
+{
+    ElemType data;
+    struct ListNode* next;
+}ListNode,*LinkList;
+
+status ListDelete(LinkList& L,int i);
