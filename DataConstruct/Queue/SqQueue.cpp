1830083bf1d8e92f58c032532f28c0741d32737f
Cui-Yancy <813158377@qq.com>
2021-12-15 11:52:28 +0800

    add SeQueue define and functions

---
+++ DataConstruct/Queue/SqQueue.cpp
@@ -0,0 +1,73 @@
+﻿#include "SqQueue.h"
+#include <iostream>
+using namespace std;
+
+status SeQueueInit(SqQueue &Q)
+{
+    Q.base = new SqQueueElemType[MAXSEQSIZE];
+    if(Q.base == NULL)
+    {
+        return ERROR;
+    }
+    Q.rear = 0;
+    Q.front = 0;
+    return OK;
+}
+
+int SeQueueLength(SqQueue Q)
+{
+    return ((Q.rear - Q.front + MAXSEQSIZE) % MAXSEQSIZE);
+}
+
+status SeQueueEnter(SqQueue &Q, SqQueueElemType elem)
+{
+    if(!Q.base)
+    {
+        return ERROR;
+    }
+    if((Q.rear+1)%MAXSEQSIZE == Q.front)    //队满
+    {
+        return ERROR;
+    }
+    else
+    {
+        Q.base[Q.rear] = elem;
+        Q.rear = (Q.rear + 1) % MAXSEQSIZE;
+        return OK;
+    }
+}
+
+status SeQueueExit(SqQueue &Q, SqQueueElemType &elem)
+{
+    if(!Q.base)
+    {
+        return ERROR;
+    }
+    if(Q.front == Q.rear)   //队列为空
+    {
+        return ERROR;
+    }
+    else
+    {
+        elem = Q.base[Q.front];
+        Q.front = (Q.front + 1) % MAXSEQSIZE;
+        return OK;
+    }
+}
+
+status SeQueueGetHead(SqQueue Q, SqQueueElemType &elem)
+{
+    if(!Q.base)
+    {
+        return ERROR;
+    }
+    if(Q.front == Q.rear)
+    {
+        return ERROR;
+    }
+    else
+    {
+        elem = Q.base[Q.front];
+        return OK;
+    }
+}
---
+++ DataConstruct/Queue/SqQueue.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "define.h"
+
+#define MAXSEQSIZE  100
+typedef int SqQueueElemType;
+typedef struct
+{
+    SqQueueElemType *base;
+    int front;
+    int rear;
+}SqQueue;
+
+status SeQueueInit(SqQueue &Q);
+status SeQueueEnter(SqQueue &Q, SqQueueElemType elem);
+int SeQueueLength(SqQueue Q);       //返回队列中的元素个数
+status SeQueueExit(SqQueue &Q, SqQueueElemType &elem);
+status SeQueueGetHead(SqQueue Q, SqQueueElemType &elem);   //获取队头元素和出队不一样，不改变队列内容
